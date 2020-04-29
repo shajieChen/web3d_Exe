ca36ada6f9753de2c4e99fb627f466eeb07952b7
SJ cheng <513601594@qq.com>
2020-04-29 17:20:07 +0800

    ‘测试javascript’

---
+++ web3d/a/helloworld.js
@@ -0,0 +1,28 @@
+// A Javascript comment
+/* Another Javascript comment,
+   intended for multi-line comments  */
+   "use strict";
+   var p = print("Hello World");
+   print(p);
+ 
+   function print(str)
+   {
+     document.write("<p>" + str + "<p>");
+     return "I printed something";
+   }
+   var text ; 
+   for (var i = 0; i < 5; i++) {
+    text += "The number is " + i + "<br>";
+  }
+  
+  var fruits = ["Banana", "Orange", "Apple", "Mango"];
+  fruits.sort();        // Sorts the elements of fruits
+
+    function my_code(){
+        alert("alert");
+    }
+    
+    window.onload=my_code();
+   //console.log("Hello world");
+   
+   

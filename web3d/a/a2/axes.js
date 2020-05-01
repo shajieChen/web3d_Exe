8290175039f0c3e4ee34da559e80d6e3668660c6
SJ cheng <513601594@qq.com>
2020-05-01 11:18:14 +0800

    'Three js'

---
+++ web3d/a/a2/axes.js
@@ -0,0 +1,29 @@
+/**
+ * Create and return a set of axes
+ */
+
+function createAxes(length)
+{
+  var geometry = new THREE.Geometry();
+  geometry.vertices.push(new THREE.Vector3(0, 0, 0));
+  geometry.vertices.push(new THREE.Vector3(length, 0, 0));
+  geometry.vertices.push(new THREE.Vector3(0, 0, 0));
+  geometry.vertices.push(new THREE.Vector3(0, length, 0));
+  geometry.vertices.push(new THREE.Vector3(0, 0, 0));
+  geometry.vertices.push(new THREE.Vector3(0, 0, length));
+  geometry.colors.push(new THREE.Color(0xff0000));
+  geometry.colors.push(new THREE.Color(0xff0000));
+  geometry.colors.push(new THREE.Color(0x00ff00));
+  geometry.colors.push(new THREE.Color(0x00ff00));
+  geometry.colors.push(new THREE.Color(0x0000ff));
+  geometry.colors.push(new THREE.Color(0x0000ff));
+  
+  var material = new THREE.LineBasicMaterial();
+  material.vertexColors = THREE.VertexColors;
+
+  var axes = new THREE.LineSegments(geometry, material);
+  axes.name = "axes";
+
+  return axes;
+}
+      
---
+++ web3d/a/a2/cube.js
@@ -0,0 +1,120 @@
+/**
+ * cube.js 
+ * A simple Three.js program which draws a cube
+ */
+
+"use strict"; 
+var scene ; 
+var camera;
+var renderer;  
+var shoulder = new THREE.Object3D();
+shoulder.name = "shoulder";
+shoulder.rotation.z = Math.PI / 6;
+
+init() ;   
+createShoulder() ;
+scene.add(shoulder);   
+scene.add(createAxes(2));
+renderer.render(scene, camera); 
+
+var controls = new THREE.TrackballControls(camera, renderer.domElement);
+controls.addEventListener('change', render);
+animate();
+
+function render() {
+    renderer.render(scene, camera); 
+}
+
+function animate() {
+    render(); 
+    requestAnimationFrame(animate);
+    controls.update();
+}
+
+function init()
+{
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, .01, 1000);
+
+//var camera = new THREE.OrthographicCamera(-1, 1, 1, -1, -1, 1);
+    camera.position.z = 2; 
+    renderer = new THREE.WebGLRenderer();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setClearColor(0x404040, 1);	// grey background 
+    document.body.appendChild(renderer.domElement); 
+    // Output to the console the threejs version
+    console.log("threejs version:" + THREE.REVISION);
+
+    document.onkeydown = handleKeyDown;
+    document.onkeyup = handleKeyUp;
+} 
+
+function createShoulder() 
+{ 
+    // Upper arm
+    var geometry = new THREE.BoxGeometry(2, 1, 1);
+    var material = new THREE.MeshBasicMaterial({color: 0xffff00});
+    var upperArm = new THREE.Mesh(geometry, material);
+    upperArm.name = "upperArm";
+    upperArm.position.x = 1;
+    upperArm.add(createAxes(2));
+    
+
+    var elbow = new THREE.Mesh();  
+    elbow.position.x = 1; 
+    elbow.add(createAxes(2));
+
+    upperArm.add(elbow);
+    geometry = new THREE.BoxGeometry(2, 1, 1);
+    material = new THREE.MeshBasicMaterial({color: 0xffff00});
+    var lowerArm = new THREE.Mesh(geometry, material);
+    lowerArm.name = "upperArm";
+    lowerArm.position.x = 0.5;
+    lowerArm.rotation.z = Math.PI / 10 ; 
+    lowerArm.add(createAxes(2));
+    elbow.add(lowerArm);
+    
+    // Add upper arm as child of shoulder 
+    shoulder.add(upperArm);
+    shoulder.add(createAxes(2));  
+}
+
+// 0xffff00
+function createCube(scaleX , scaleY, scaleZ,  
+                    hex16Color , isWireFrameOn)
+{ 
+    // A square using vertex coordinates and face indexes
+    var geometry = new THREE.Geometry();
+    geometry.vertices.push(new THREE.Vector3(-1, -1, 0));
+    geometry.vertices.push(new THREE.Vector3(1, -1, 0));
+    geometry.vertices.push(new THREE.Vector3(1, 1, 0));
+    geometry.vertices.push(new THREE.Vector3(-1, 1, 0));
+    geometry.faces.push(new THREE.Face3(0, 1, 2));
+    geometry.faces.push(new THREE.Face3(0, 2, 3));
+    geometry.computeFaceNormals();
+    var material = new THREE.MeshBasicMaterial({color: hex16Color}); 
+    var object = new THREE.Mesh(geometry, material); 
+    object.rotation.z = Math.PI / 4 ; 
+    object.material.wireframe = isWireFrameOn ;   
+    object.scaleX = scaleX ; 
+    object.scaleY = scaleY;  
+    object.scaleZ = scaleZ;  
+    return object;  
+}  
+
+function handleKeyDown(event)
+{
+    switch (event.keyCode) {
+    case 37:
+        scene.rotation.z += 1 * Math.PI / 180;
+        break;
+    case 39:
+        scene.rotation.z -= 1 * Math.PI / 180;
+        break;
+    }
+}
+function handleKeyUp(event)
+{
+    // Details go here
+}
